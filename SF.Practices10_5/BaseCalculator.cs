62fe0bfcd8b556b657d6714eac647d7afbf84f80
Irina Lokosova <persik2466@gmail.com>
2021-11-02 20:24:38 +0400

    Практикум 10.5. Интерфейсы. Калькулятор.

---
+++ SF.Practices10_5/BaseCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+namespace SF.Practices10_5
+{/// <summary>
+/// Класс калькулятор - наследник интерфейсов
+/// </summary>
+    public class BaseCalculator : ICalculator, IException
+    {
+        ILogger Logger { get; }
+        public BaseCalculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+        /// <summary>
+        /// Метод сложения двух чисел
+        /// </summary>
+        /// <param name="numberOne"></param>
+        /// <param name="numberTwo"></param>
+        public void Solve(int numberOne, int numberTwo)
+        {
+            Logger.Event($"Сумма чисел {numberOne} и {numberTwo} равна {numberOne + numberTwo}");
+        }
+        /// <summary>
+        /// Метод для обработки ошибок
+        /// </summary>
+        public void ExpErr()
+        {
+            Logger.Error("Введено некорректное значение, вводите числа!");
+        }
+        /// <summary>
+        /// Метод базового цвета
+        /// </summary>
+        public void FinColor()
+        {
+            Logger.BaseColor();
+        }
+    }
+}
+
--- SF.Practices10_5/Program.cs
+++ SF.Practices10_5/Program.cs
@@ -4,9 +4,78 @@ namespace SF.Practices10_5
 {
     class Program
     {
+        static ILogger Logger { get; set; }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Logger = new Logger();
+            var calc = new BaseCalculator(Logger);
+            Console.WriteLine("Калькулятор сложения 2-х чисел.");
+            while (true)
+            {
+                Console.WriteLine();
+                try
+                {
+                    Console.WriteLine("Введите первое число:");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите второе число:");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+                    calc.Solve(number1, number2);
+                }
+                catch (FormatException)
+                {
+                    calc.ExpErr();
+                }
+                finally
+                {
+                    calc.FinColor();
+                }
+
+            }
         }
     }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+        void BaseColor();
+    }
+    /// <summary>
+    /// Интерфейс для механизма внедрения зависимостей
+    /// </summary>
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+        public void BaseColor()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+
+    /// <summary>
+    /// Интерфейс для калькулятора
+    /// </summary>
+    public interface ICalculator
+    {
+        void Solve(int numberOne, int numberTwo);
+
+    }
+    /// <summary>
+    /// Интерфейс для цвета
+    /// </summary>
+    public interface IException
+    {
+        void ExpErr();
+        void FinColor();
+    }
+
 }
